dc19a3a4e613c92cda80f83386f235380888302f
Jc-skp <113411979+Jc-skp@users.noreply.github.com>
2025-10-02 21:09:24 -0300

    primeiro_commit

---
+++ appImc/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appImc',
+  webDir: 'dist'
+};
+
+export default config;
